5547ae0453d6fd9eafd23fb42184012c043f5dd9
yash9650 <yash16022001@gmail.com>
2022-04-18 12:02:08 +0530

    added logic to detail screen

--- src/MyComponents/Details.jsx
+++ src/MyComponents/Details.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
+import { Link ,useLocation} from 'react-router-dom';
 
 export default function Details() {
+  const location = useLocation();
   return (
 
     <div className="detail-page">
       <div className="container productPage col-12">
-          <div className="productPageLeft my-5 col-5 px-3 py-5"></div>
+          <div className='productPageLeft my-5 col-7 p-5'>
+            <img src={location.state.image} alt="" />
+          </div>
           <div className="productPageRight my-5 col-7 p-5">
-            <h2>Mamaearth Onion Shampoo</h2>
-            <h4>Hair Growth & Hair Fall Control with Onion Oil & Plant Keratin 400ml</h4>
-            <p className='py-3'>Onion Oil stimulates the scalp, promoting blood circulation and hair growth. Plant Keratin strengthens hair, and makes it frizz-free.</p>
+            <h2>{location.state.title}</h2>
+            {/* <h4>Hair Growth & Hair Fall Control with Onion Oil & Plant Keratin 400ml</h4> */}
+            <p className='py-3'>{location.state.desc}</p>
             <p className="pt-3"><span className="cut-price">&#x20B9;699</span> (20% off) </p>
             <form>
               <p><span className="price">&#x20B9;560</span> | <span className="in-stock">In stock</span> </p>
