8a00b6b43456bb163e1422652b976120ee4fbc42
rishitaprakash <rishitaprakash3105@gmail.com>
2022-05-05 22:25:50 +0530

    home

--- src/MyComponents/Groceries.jsx
+++ src/MyComponents/Groceries.jsx
@@ -6,20 +6,7 @@ export default function Groceries(props) {
 
   return <>
     <div className="header" style={{ backgroundImage: `url(${img1})` }}></div>
-<<<<<<< HEAD
-    <section className="section-g">
-      <div className="container py-5">
-
-        <div className="row d-inline-flex justify-content-center">
-          {
-            card
-          }
-        </div>
-      </div>
-    </section>
-=======
   <DisplayCard currentUser={props.currentUser} itemData={props.itemData} type1='groceries' type2 = ''/>
->>>>>>> 5709e660826243deab00062c5c274864758669fd
   </>
   
 }
